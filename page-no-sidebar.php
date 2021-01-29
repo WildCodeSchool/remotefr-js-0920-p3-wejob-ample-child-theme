794d466c829c50af99c25b48fa4811f5e2ac5f2b
Benoit Hubert <benoithubert@gmail.com>
2021-01-29 07:03:17 +0000

    Init Ample child theme

---
+++ page-no-sidebar.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template Name: No Sidebar
+ *
+ * Page without sidebar.
+ *
+ * @package ThemeGrill
+ * @subpackage Ample
+ * @since Ample 0.1
+ */
+
+get_header();
+
+   do_action( 'ample_before_body_content' ); ?>
+
+   <div class="single-page clearfix">
+      <div class="inner-wrap">
+
+       <?php while ( have_posts() ) : the_post(); ?>
+
+	  <?php get_template_part( 'content', 'page' ); ?>
+
+	  <?php
+	     do_action( 'ample_before_comments_template' );
+	     // If comments are open or we have at least one comment, load up the comment template
+	     if ( comments_open() || '0' != get_comments_number() )
+		comments_template();
+	     do_action ( 'ample_after_comments_template' );
+	  ?>
+       <?php endwhile; ?>
+
+      </div><!-- .inner-wrap -->
+   </div><!-- .single-page -->
+
+   <?php do_action( 'ample_after_body_content' );
+get_footer(); ?>
